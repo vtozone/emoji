cc10377ae566c35c480251d56d31357287817128
litangchuns@sina.com <litangchuns@sina.com>
2015-11-23 23:52:12 +0800

    首次导入

---
+++ WebContent/js/emoji.json.js
@@ -0,0 +1,538 @@
+/**
+ * emoji.json.js
+ */
+;
+var map = new Map();
+
+map.put('rabbit', [ {
+	'value' : '',
+	'url' : '16.gif'
+}, {
+	'value' : '',
+	'url' : '19.gif'
+}, {
+	'value' : '',
+	'url' : '27.gif'
+}, {
+	'value' : '',
+	'url' : '30.gif'
+}, {
+	'value' : '',
+	'url' : '02.gif'
+}, {
+	'value' : '',
+	'url' : '24.gif'
+}, {
+	'value' : '',
+	'url' : '08.gif'
+}, {
+	'value' : '',
+	'url' : '14.gif'
+}, {
+	'value' : '',
+	'url' : '22.gif'
+}, {
+	'value' : '',
+	'url' : '26.gif'
+}, {
+	'value' : '',
+	'url' : '25.gif'
+}, {
+	'value' : '',
+	'url' : '10.gif'
+}, {
+	'value' : '',
+	'url' : '09.gif'
+}, {
+	'value' : '',
+	'url' : '07.gif'
+}, {
+	'value' : '',
+	'url' : '29.gif'
+}, {
+	'value' : '',
+	'url' : '04.gif'
+}, {
+	'value' : '',
+	'url' : '18.gif'
+}, {
+	'value' : '',
+	'url' : '23.gif'
+}, {
+	'value' : '',
+	'url' : '20.gif'
+}, {
+	'value' : '',
+	'url' : '03.gif'
+}, {
+	'value' : '',
+	'url' : '12.gif'
+}, {
+	'value' : '',
+	'url' : '28.gif'
+}, {
+	'value' : '',
+	'url' : '21.gif'
+}, {
+	'value' : '',
+	'url' : '01.gif'
+}, {
+	'value' : '',
+	'url' : '15.gif'
+}, {
+	'value' : '',
+	'url' : '05.gif'
+}, {
+	'value' : '',
+	'url' : '17.gif'
+}, {
+	'value' : '',
+	'url' : '06.gif'
+}, {
+	'value' : '',
+	'url' : '13.gif'
+}, {
+	'value' : '',
+	'url' : '11.gif'
+} ]);
+map.put('leon', [ {
+	'value' : '',
+	'url' : '16.gif'
+}, {
+	'value' : '',
+	'url' : '37.gif'
+}, {
+	'value' : '',
+	'url' : '59.gif'
+}, {
+	'value' : '',
+	'url' : '19.gif'
+}, {
+	'value' : '',
+	'url' : '27.gif'
+}, {
+	'value' : '',
+	'url' : '34.gif'
+}, {
+	'value' : '',
+	'url' : '48.gif'
+}, {
+	'value' : '',
+	'url' : '30.gif'
+}, {
+	'value' : '',
+	'url' : '44.gif'
+}, {
+	'value' : '',
+	'url' : '68.gif'
+}, {
+	'value' : '',
+	'url' : '46.gif'
+}, {
+	'value' : '',
+	'url' : '66.gif'
+}, {
+	'value' : '',
+	'url' : '02.gif'
+}, {
+	'value' : '',
+	'url' : '55.gif'
+}, {
+	'value' : '',
+	'url' : '58.gif'
+}, {
+	'value' : '',
+	'url' : '63.gif'
+}, {
+	'value' : '',
+	'url' : '41.gif'
+}, {
+	'value' : '',
+	'url' : '67.gif'
+}, {
+	'value' : '',
+	'url' : '24.gif'
+}, {
+	'value' : '',
+	'url' : '08.gif'
+}, {
+	'value' : '',
+	'url' : '14.gif'
+}, {
+	'value' : '',
+	'url' : '52.gif'
+}, {
+	'value' : '',
+	'url' : '65.gif'
+}, {
+	'value' : '',
+	'url' : '22.gif'
+}, {
+	'value' : '',
+	'url' : '36.gif'
+}, {
+	'value' : '',
+	'url' : '42.gif'
+}, {
+	'value' : '',
+	'url' : '26.gif'
+}, {
+	'value' : '',
+	'url' : '25.gif'
+}, {
+	'value' : '',
+	'url' : '10.gif'
+}, {
+	'value' : '',
+	'url' : '09.gif'
+}, {
+	'value' : '',
+	'url' : '07.gif'
+}, {
+	'value' : '',
+	'url' : '64.gif'
+}, {
+	'value' : '',
+	'url' : '49.gif'
+}, {
+	'value' : '',
+	'url' : '39.gif'
+}, {
+	'value' : '',
+	'url' : '54.gif'
+}, {
+	'value' : '',
+	'url' : '29.gif'
+}, {
+	'value' : '',
+	'url' : '04.gif'
+}, {
+	'value' : '',
+	'url' : '47.gif'
+}, {
+	'value' : '',
+	'url' : '18.gif'
+}, {
+	'value' : '',
+	'url' : '61.gif'
+}, {
+	'value' : '',
+	'url' : '23.gif'
+}, {
+	'value' : '',
+	'url' : '35.gif'
+}, {
+	'value' : '',
+	'url' : '20.gif'
+}, {
+	'value' : '',
+	'url' : '60.gif'
+}, {
+	'value' : '',
+	'url' : '50.gif'
+}, {
+	'value' : '',
+	'url' : '03.gif'
+}, {
+	'value' : '',
+	'url' : '62.gif'
+}, {
+	'value' : '',
+	'url' : '12.gif'
+}, {
+	'value' : '',
+	'url' : '56.gif'
+}, {
+	'value' : '',
+	'url' : '28.gif'
+}, {
+	'value' : '',
+	'url' : '57.gif'
+}, {
+	'value' : '',
+	'url' : '21.gif'
+}, {
+	'value' : '',
+	'url' : '01.gif'
+}, {
+	'value' : '',
+	'url' : '33.gif'
+}, {
+	'value' : '',
+	'url' : '53.gif'
+}, {
+	'value' : '',
+	'url' : '40.gif'
+}, {
+	'value' : '',
+	'url' : '15.gif'
+}, {
+	'value' : '',
+	'url' : '31.gif'
+}, {
+	'value' : '',
+	'url' : '05.gif'
+}, {
+	'value' : '',
+	'url' : '17.gif'
+}, {
+	'value' : '',
+	'url' : '43.gif'
+}, {
+	'value' : '',
+	'url' : '45.gif'
+}, {
+	'value' : '',
+	'url' : '38.gif'
+}, {
+	'value' : '',
+	'url' : '06.gif'
+}, {
+	'value' : '',
+	'url' : '13.gif'
+}, {
+	'value' : '',
+	'url' : '11.gif'
+}, {
+	'value' : '',
+	'url' : '51.gif'
+}, {
+	'value' : '',
+	'url' : '32.gif'
+} ]);
+map.put('weibo', [ {
+	'value' : '',
+	'url' : '16.gif'
+}, {
+	'value' : '',
+	'url' : '37.gif'
+}, {
+	'value' : '',
+	'url' : '19.gif'
+}, {
+	'value' : '',
+	'url' : '27.gif'
+}, {
+	'value' : '',
+	'url' : '34.gif'
+}, {
+	'value' : '',
+	'url' : '48.gif'
+}, {
+	'value' : '',
+	'url' : '30.gif'
+}, {
+	'value' : '',
+	'url' : '44.gif'
+}, {
+	'value' : '',
+	'url' : '46.gif'
+}, {
+	'value' : '',
+	'url' : '02.gif'
+}, {
+	'value' : '',
+	'url' : '41.gif'
+}, {
+	'value' : '',
+	'url' : '24.gif'
+}, {
+	'value' : '',
+	'url' : '08.gif'
+}, {
+	'value' : '',
+	'url' : '14.gif'
+}, {
+	'value' : '',
+	'url' : '22.gif'
+}, {
+	'value' : '',
+	'url' : '36.gif'
+}, {
+	'value' : '',
+	'url' : '42.gif'
+}, {
+	'value' : '',
+	'url' : '26.gif'
+}, {
+	'value' : '',
+	'url' : '25.gif'
+}, {
+	'value' : '',
+	'url' : '10.gif'
+}, {
+	'value' : '',
+	'url' : '09.gif'
+}, {
+	'value' : '',
+	'url' : '07.gif'
+}, {
+	'value' : '',
+	'url' : '39.gif'
+}, {
+	'value' : '',
+	'url' : '29.gif'
+}, {
+	'value' : '',
+	'url' : '04.gif'
+}, {
+	'value' : '',
+	'url' : '47.gif'
+}, {
+	'value' : '',
+	'url' : '18.gif'
+}, {
+	'value' : '',
+	'url' : '23.gif'
+}, {
+	'value' : '',
+	'url' : '35.gif'
+}, {
+	'value' : '',
+	'url' : '20.gif'
+}, {
+	'value' : '',
+	'url' : '03.gif'
+}, {
+	'value' : '',
+	'url' : '12.gif'
+}, {
+	'value' : '',
+	'url' : '28.gif'
+}, {
+	'value' : '',
+	'url' : '21.gif'
+}, {
+	'value' : '',
+	'url' : '01.gif'
+}, {
+	'value' : '',
+	'url' : '33.gif'
+}, {
+	'value' : '',
+	'url' : '40.gif'
+}, {
+	'value' : '',
+	'url' : '15.gif'
+}, {
+	'value' : '',
+	'url' : '31.gif'
+}, {
+	'value' : '',
+	'url' : '05.gif'
+}, {
+	'value' : '',
+	'url' : '17.gif'
+}, {
+	'value' : '',
+	'url' : '43.gif'
+}, {
+	'value' : '',
+	'url' : '45.gif'
+}, {
+	'value' : '',
+	'url' : '38.gif'
+}, {
+	'value' : '',
+	'url' : '06.gif'
+}, {
+	'value' : '',
+	'url' : '13.gif'
+}, {
+	'value' : '',
+	'url' : '11.gif'
+}, {
+	'value' : '',
+	'url' : '32.gif'
+} ]);
+map.put('qiubi', [ {
+	'value' : '',
+	'url' : '16.gif'
+}, {
+	'value' : '',
+	'url' : '19.gif'
+}, {
+	'value' : '',
+	'url' : '27.gif'
+}, {
+	'value' : '',
+	'url' : '30.gif'
+}, {
+	'value' : '',
+	'url' : '02.gif'
+}, {
+	'value' : '',
+	'url' : '24.gif'
+}, {
+	'value' : '',
+	'url' : '08.gif'
+}, {
+	'value' : '',
+	'url' : '14.gif'
+}, {
+	'value' : '',
+	'url' : '22.gif'
+}, {
+	'value' : '',
+	'url' : '26.gif'
+}, {
+	'value' : '',
+	'url' : '25.gif'
+}, {
+	'value' : '',
+	'url' : '10.gif'
+}, {
+	'value' : '',
+	'url' : '09.gif'
+}, {
+	'value' : '',
+	'url' : '07.gif'
+}, {
+	'value' : '',
+	'url' : '29.gif'
+}, {
+	'value' : '',
+	'url' : '04.gif'
+}, {
+	'value' : '',
+	'url' : '18.gif'
+}, {
+	'value' : '',
+	'url' : '23.gif'
+}, {
+	'value' : '',
+	'url' : '20.gif'
+}, {
+	'value' : '',
+	'url' : '03.gif'
+}, {
+	'value' : '',
+	'url' : '12.gif'
+}, {
+	'value' : '',
+	'url' : '28.gif'
+}, {
+	'value' : '',
+	'url' : '21.gif'
+}, {
+	'value' : '',
+	'url' : '01.gif'
+}, {
+	'value' : '',
+	'url' : '15.gif'
+}, {
+	'value' : '',
+	'url' : '05.gif'
+}, {
+	'value' : '',
+	'url' : '17.gif'
+}, {
+	'value' : '',
+	'url' : '06.gif'
+}, {
+	'value' : '',
+	'url' : '13.gif'
+}, {
+	'value' : '',
+	'url' : '11.gif'
+} ]);
